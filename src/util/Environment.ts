c059a5cbf72e43b86fb98526f547403a85172462
Contrabang <91113370+Contrabang@users.noreply.github.com>
2025-07-08 08:53:05 -0700

    Refactors developer checks (#8)
    * Refactors dev checks
    
    * fix
    
    * formatting
    
    ---------
    
    Co-authored-by: defaultUser <defaultUser@ema.il>

--- src/util/Environment.ts
+++ src/util/Environment.ts
@@ -1,3 +1,5 @@
+import { getPwGameWorldHelper } from '@/store/PWClientStore.ts'
+
 export function getEnvDefaultWorldId() {
   if (!import.meta.env.VITE_DEFAULT_WORLD_ID) {
     throw new Error('VITE_DEFAULT_WORLD_ID is not defined in environment variables')
@@ -10,4 +12,18 @@ export function isEnvDevViewEnabled() {
     throw new Error('VITE_DEV_VIEW is not defined in environment variables')
   }
   return import.meta.env.VITE_DEV_VIEW === 'TRUE'
+}
+
+export function isDeveloper(playerId: number) {
+  const player = getPwGameWorldHelper().getPlayer(playerId)
+  if (!player) {
+    return false
+  }
+  let dev_username = 'PIRATUX'
+  if (import.meta.env.VITE_DEV_USERNAME) {
+    dev_username = import.meta.env.VITE_DEV_USERNAME.toUpperCase()
+  } else {
+    console.log('VITE_DEV_USERNAME is not defined in environment variables, assuming "PIRATUX"')
+  }
+  return player.username === dev_username
 }
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
   readonly VITE_DEFAULT_WORLD_ID: string
   readonly VITE_DEV_VIEW: string
+  readonly VITE_DEV_USERNAME: string
 }
 
 interface ImportMeta {
