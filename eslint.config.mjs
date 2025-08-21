24ad20fe492cfa37d9153b4a35b6ae955088f59e
manali <manali@novian.lt>
2025-08-21 23:56:08 +0300

    Add back eslint-plugin-vue due to false alarm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
+import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 
 // TODO: Consider moving to Oxlint or Biome in the future to improve linting speed.
 //  At the time of writing (2025-08-15), Oxlint and Biome has poor Vue and type checked rules support.
 export default defineConfigWithVueTs(
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommendedTypeChecked,
   vueTsConfigs.stylisticTypeChecked,
   {
