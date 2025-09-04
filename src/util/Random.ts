8c339559cfdbab7c57c88e41b4cdbaf12a036334
Piratux <58703216+Piratux@users.noreply.github.com>
2025-09-04 21:32:38 +0300

    Cleanup and fixes

--- src/util/Random.ts
+++ src/util/Random.ts
@@ -1,21 +1,4 @@
-import { setTimeout as workerSetTimeout } from 'worker-timers'
-
-// NOTE: workerSetTimeout makes sure sleeps are not throttled in inactive tabs (this is required as bot is hosted on browser)
-
-/**
- * https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
- */
-export function sleep(ms = 0): Promise<void> {
-  return new Promise((resolve) => {
-    workerSetTimeout(resolve, ms)
-  })
-}
-
-export function setCustomTimeout(callback: () => void, ms = 0): void {
-  workerSetTimeout(callback, ms)
-}
-
 // [min, max)
 export function getRandomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * max) + min
+  return Math.floor(Math.random() * (max - min)) + min
 }
