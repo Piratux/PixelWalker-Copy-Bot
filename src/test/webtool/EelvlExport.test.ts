ffa51a6c3a314742ae4ed1f7ac13b912ee35d22d
Piratux <58703216+Piratux@users.noreply.github.com>
2025-11-09 18:37:19 +0200

    Add EELVL Import test

--- src/test/webtool/EelvlExport.test.ts
+++ src/test/webtool/EelvlExport.test.ts
@@ -1,17 +1,12 @@
-import { beforeEach, describe, test } from 'vitest'
+import { describe, test } from 'vitest'
 import { getPwApiClient } from '@/core/store/PwClientStore.ts'
 import { compareDeserialisedStructureData } from '@/test/RuntimeTestsUtil.ts'
-import { clearWorld } from '@/core/service/PwClientService.ts'
 import { getAnotherWorldBlocks } from '@/core/service/WorldService.ts'
 import { getExportedToEelvlData } from '@/webtool/eelvl/service/EelvlExporterService.ts'
 import { getImportedFromEelvlData } from '@/webtool/eelvl/service/EelvlImporterService.ts'
 import { bufferToArrayBuffer } from '@/core/util/Buffers.ts'
 
 describe.sequential('EELVL Export', () => {
-  beforeEach(async () => {
-    await clearWorld()
-  })
-
   test('EELVL Export (with EELVL Import)', { timeout: 60_000 }, async () => {
     const testEelvlExportWorldId = 'ra9285102d4a41a'
     const expectedData = await getAnotherWorldBlocks(testEelvlExportWorldId, getPwApiClient())
