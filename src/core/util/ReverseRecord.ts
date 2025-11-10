b15d31cfb9f1150826a4cd1ef6152c0b86b916d4
Piratux <58703216+Piratux@users.noreply.github.com>
2025-11-10 23:01:14 +0200

    Remove unused ReverseRecord.ts

--- src/core/util/ReverseRecord.ts
+++
@@ -1,3 +0,0 @@
-export function getReversedRecord<T extends PropertyKey, U extends PropertyKey>(input: Record<T, U>) {
-  return Object.fromEntries(Object.entries(input).map(([key, value]) => [value, key])) as Record<U, T>
-}
